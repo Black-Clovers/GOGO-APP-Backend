446594ce3db4368457d06909395957aa57d185c5
samitha senevirathne <88784541+samitha99429@users.noreply.github.com>
2022-09-02 11:08:21 +0530

    Feature/it20607678 (#10)
    * Feat(gitignore) : create file
    
    * Feat(project) :  init project
    
    * feat(project) : folder setup
    
    * Vacancy create (#8)
    
    * create travel package routes model controller (#6)
    
    * create bcakend
    
    Co-authored-by: Tharindu Thilakarathna <84170568+tharinduthilakarathna98@users.noreply.github.com>
    
    * Remove .env file and .gitkeep files (#12)
    
    * create travel package routes model controller (#6)
    
    * create bcakend
    
    * #commit :  remove .env file and .gitkeep files
    
    Co-authored-by: Tharindu Thilakarathna <84170568+tharinduthilakarathna98@users.noreply.github.com>
    
    Co-authored-by: Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
    Co-authored-by: Tharindu Thilakarathna <84170568+tharinduthilakarathna98@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const logger = require("pino")();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const expressSession = require("express-session");
+
+const app = express();
+dotenv.config();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.set("trust proxy", 1);
+const sessSettings = expressSession({
+  path: "/",
+  secret: "oursecret",
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    sameSite: false,
+    secure: false,
+    maxAge: 360000,
+  },
+});
+
+app.use(sessSettings);
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  logger.info(" Mongodb connected successfully");
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({ messsage: "Server is running!" });
+});
+
+app.use("/api/package", require("./routes/travelPackageRoutes"));
+app.use("/api/vacancy",require("./routes/CareerRoutes"))
+
+
+app.listen(PORT, () => {
+  logger.info(`Server is running on PORT: ${PORT}`);
+});
+
+
+
+
