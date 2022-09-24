3f7fe7d0d8663296785000f8b65899639f239100
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-09-24 14:55:01 +0530

    feat(Model) : Ride model done

---
+++ model/RideModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const rideSchema = new Schema({
+	trip_ID: { type: String, required: true },
+	trip_Type: { type: String, required: true },
+	trip_pickUp_Location: { type: Object, required: true },
+	trip_dropOff_Location: { type: Object, required: true },
+	trip_pickUp_Date: { type: String, required: true },
+	trip_pickUp_Time: { type: String, required: true },
+	trip_vehicle_Type: { type: String, required: true },
+	trip_Status: { type: String, required: true },
+});
+
+const RideModel = mongoose.model("RideModel", rideSchema);
+module.exports = RideModel;
